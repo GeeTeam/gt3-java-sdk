b68761bb2c850c2cdc008c4ba1dc2bf79d0be4f5
Aisaka <383449573@qq.com>
2017-03-20 15:42:14 +0800

    first commit

---
+++ src/demo/demo2/GeetestConfig.java
@@ -0,0 +1,29 @@
+package demo.demo2;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+/**
+ * GeetestWeb配置文件
+ * 
+ *
+ */
+public class GeetestConfig {
+
+	// 填入自己的captcha_id和private_key
+	private static final String geetest_id = "002bc30ff1eef93e912f45814945e752";
+	private static final String geetest_key = "4193a0e3247b82a26f563d595c447b1a";
+	private static final boolean newfailback = true;
+
+	public static final String getGeetest_id() {
+		return geetest_id;
+	}
+
+	public static final String getGeetest_key() {
+		return geetest_key;
+	}
+	
+	public static final boolean isnewfailback() {
+		return newfailback;
+	}
+
+}
